9de8fa4ec27c73aa3e5d848cdb1fd7a6cfb25baa
DeepNetwork <mydeepnetwork@gmail.com>
2018-06-19 21:12:28 +0300

    ToolTip & Popup

--- WPFApplication/MainWindow.xaml.cs
+++ WPFApplication/MainWindow.xaml.cs
@@ -23,50 +23,22 @@ namespace WPFApplication
         public MainWindow()
         {
             InitializeComponent();
-            CreateCheckBox();
-            CreateRadioButton();
+            CreateToolTipForButton();
         }
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        private void CreateToolTipForButton()
         {
-            MessageBox.Show(((CheckBox)sender).Content.ToString() + " отмечен");
+            ToolTip toolTip = new ToolTip();
+            StackPanel toolTipPanel = new StackPanel();
+            toolTipPanel.Children.Add(new TextBlock { Text = "Header", FontSize = 20 });
+            toolTipPanel.Children.Add(new TextBlock { Text = "Some Text" });
+            toolTip.Content = toolTipPanel;
+            buttonToolTip.ToolTip = toolTip;
         }
 
-        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
         {
-            MessageBox.Show(((CheckBox)sender).Content.ToString() + " не отмечен");
-        }
-
-        private void CheckBox_Indeterminate(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(((CheckBox)sender).Content.ToString() + " в неопределенном состоянии");
-        }
-
-        private void CreateCheckBox()
-        {
-            CheckBox checkBox4 = new CheckBox { Content = "Флажок С#", Height = 30, IsChecked = false, Margin = new Thickness(20, 0, 0, 0), IsThreeState=true };
-            checkBox4.Checked += CheckBox_Checked;
-            checkBox4.Unchecked += CheckBox_Unchecked;
-            checkBox4.Indeterminate += CheckBox_Indeterminate;
-            stackPanel1.Children.Add(checkBox4);
-        }
-
-        private void CreateRadioButton()
-        {
-            RadioButton radioButton7 = new RadioButton { Content = "Console", Height = 20, Margin = new Thickness(20, 0, 0, 0), GroupName = "Technologies" };
-            radioButton7.Checked        += RadioButton_Checked;
-            radioButton7.Unchecked      += RadioButton_Unchecked;
-            stackPanel2.Children.Add(radioButton7);
-        }
-
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(((RadioButton)sender).Content.ToString() + " отмечен");
-        }
-
-        private void RadioButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(((RadioButton)sender).Content.ToString() + " не отмечен");
+            popup.IsOpen = true;
         }
     }
 }
