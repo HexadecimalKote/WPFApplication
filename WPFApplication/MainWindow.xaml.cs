cc9448929f226bacf5caf4080ec0d04ac6d931ab
DeepNetwork <mydeepnetwork@gmail.com>
2018-06-19 16:34:06 +0300

    ContentControl

--- WPFApplication/MainWindow.xaml.cs
+++ WPFApplication/MainWindow.xaml.cs
@@ -23,6 +23,21 @@ namespace WPFApplication
         public MainWindow()
         {
             InitializeComponent();
+            double bText = 4.05;
+            Button4.Content = bText;
+            CreateButton7();
+        }
+
+        private void CreateButton7()
+        {
+            Button button7 = new Button();
+            StackPanel stackPanel = new StackPanel();
+            stackPanel.Children.Add(new TextBlock { Text = "Набор кнопок C#" });
+            stackPanel.Children.Add(new Button { Content = "Blue", Height = 20, Background = new SolidColorBrush(Colors.Blue) });
+            stackPanel.Children.Add(new Button { Content = "Yellow", Height = 20, Background = new SolidColorBrush(Colors.Yellow) });
+            stackPanel.Children.Add(new Button { Content = "Magenta", Height = 20, Background = new SolidColorBrush(Colors.Magenta) });
+            button7.Content = stackPanel;
+            FirstStackPanel.Children.Add(button7);
         }
     }
 }
