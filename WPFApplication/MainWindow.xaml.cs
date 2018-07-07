1af0e66eef8fff0d4844b9404d788f7248090b22
DeepNetwork <mydeepnetwork@gmail.com>
2018-07-07 22:34:11 +0300

    DataGrid

--- WPFApplication/MainWindow.xaml.cs
+++ WPFApplication/MainWindow.xaml.cs
@@ -10,21 +10,44 @@ namespace WPFApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Phone> listPhones;
+
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-        private void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
-        {
-            TreeViewItem tvItem = (TreeViewItem)sender;
-            MessageBox.Show(tvItem.Header.ToString() + " раскрыт");
-        }
+            listPhones = new List<Phone>
+            {
+                new Phone { Title="iPhone 6S", Company="Apple", Price=54990 },
+                new Phone {Title="Lumia 950", Company="Microsoft", Price=39990 },
+                new Phone {Title="Nexus 5X", Company="Google", Price=29990 }
+            };
 
-        private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
-        {
-            TreeViewItem tvItem = (TreeViewItem)sender;
-            MessageBox.Show("Выбран узел: " + tvItem.Header.ToString());
+            /*
+           RowBackground и AlternatingRowBackground Устанавливают фон строки. 
+             Если установлены оба свойства, цветовой фон чередуется: 
+             RowBackground - для нечетных строк и AlternatingRowBackground - для четных
+
+           ColumnHeaderHeight - Устанавливает высоту строки названий столбцов.
+
+           ColumnWidth - Устанавливает ширину столбцов.
+
+           RowHeight - Устанавливает высоту строк.
+
+           GridLinesVisibility Устанавливает видимость линий, разделяющих столбцы и строки. 
+           Имеет четыре значения -
+                All - видны все линии, 
+                Horizontal - видны только горизонтальные линии, 
+                Vertical - видны только вертикальные линии, 
+                None - линии отсутствуют
+
+           HeadersVisibility - Задает видимость заголовков
+
+           HorizontalGridLinesBrush и VerticalGridLinesBrush
+           Задает цвет горизонтальных и вертикальных линий соответственно
+             */
+            phonesGrid_1.ItemsSource = listPhones;
+            phonesGrid_1.ColumnHeaderHeight = 25;
         }
     }
 }
