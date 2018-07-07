9bb9fe3ea46a6d6c99ec537e344e8b1a12b55b60
DeepNetwork <mydeepnetwork@gmail.com>
2018-07-07 22:51:42 +0300

    ProgressBar and Slider

--- WPFApplication/MainWindow.xaml.cs
+++ WPFApplication/MainWindow.xaml.cs
@@ -10,44 +10,14 @@ namespace WPFApplication
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Phone> listPhones;
-
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();   
+        }
 
-            listPhones = new List<Phone>
-            {
-                new Phone { Title="iPhone 6S", Company="Apple", Price=54990 },
-                new Phone {Title="Lumia 950", Company="Microsoft", Price=39990 },
-                new Phone {Title="Nexus 5X", Company="Google", Price=29990 }
-            };
-
-            /*
-           RowBackground и AlternatingRowBackground Устанавливают фон строки. 
-             Если установлены оба свойства, цветовой фон чередуется: 
-             RowBackground - для нечетных строк и AlternatingRowBackground - для четных
-
-           ColumnHeaderHeight - Устанавливает высоту строки названий столбцов.
-
-           ColumnWidth - Устанавливает ширину столбцов.
-
-           RowHeight - Устанавливает высоту строк.
-
-           GridLinesVisibility Устанавливает видимость линий, разделяющих столбцы и строки. 
-           Имеет четыре значения -
-                All - видны все линии, 
-                Horizontal - видны только горизонтальные линии, 
-                Vertical - видны только вертикальные линии, 
-                None - линии отсутствуют
-
-           HeadersVisibility - Задает видимость заголовков
-
-           HorizontalGridLinesBrush и VerticalGridLinesBrush
-           Задает цвет горизонтальных и вертикальных линий соответственно
-             */
-            phonesGrid_1.ItemsSource = listPhones;
-            phonesGrid_1.ColumnHeaderHeight = 25;
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ((Slider)sender).SelectionEnd = e.NewValue;
         }
     }
 }
